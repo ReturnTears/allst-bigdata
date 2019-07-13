42bc19c58bfe8f48b7f93a306378a0a38009b729
returntears <returntears@outlook.com>
2019-07-13 13:03:17 +0800

    Scala学习

---
+++ src/main/scala/com/allst/scala/comm_use/MaxValue_Java.java
@@ -0,0 +1,27 @@
+package com.allst.scala.comm_use;
+
+/**
+ * java在序列中查找最大或最小值
+ * @Auther JUNN
+ * @Date 2019-07-13 下午 12:25
+ */
+public class MaxValue_Java {
+    public static void main(String[] args) {
+        evalMaxVal();
+    }
+
+    /**
+     * 计算最大值
+     */
+    private static void evalMaxVal() {
+        int[] arr = {11, 2, 5, 1, 6, 3, 9, 17, 22};
+        int to = arr.length - 1;
+        int max = arr[0];
+        for (int i = 0; i < to; i++) {
+            if (max < arr[i + 1]) {
+                max = arr[i + 1];
+            }
+        }
+        System.out.println("Max Value : " + max);
+    }
+}
---
+++ src/main/scala/com/allst/scala/comm_use/MaxValue_Scala.scala
@@ -0,0 +1,34 @@
+package com.allst.scala.comm_use
+
+/**
+  * Scala在序列中获取最大值
+  * @Auther JUNN
+  * @Date 2019-07-13 下午 12:49
+  */
+object MaxValue_Scala {
+  def main(args: Array[String]): Unit = {
+    new MaxValue().maxVal()
+  }
+}
+// 计算最大值类
+class MaxValue {
+  def maxVal(): Unit = {
+    val numbers = Seq(11, 2, 5, 1, 6, 3, 9, 17, 22)
+    println(numbers.max)
+    println(numbers.min)
+  }
+
+  // 增加操作数据的复杂度
+  case class Book(title: String, pages: Int)
+
+  val books = Seq(
+    Book("Future of Scala developers", 85),
+    Book("Parallel algorithms", 240),
+    Book("Object Oriented Programming", 130),
+    Book("Mobile Development", 495)
+  )
+  val max = books.maxBy(book => book.pages)
+  println("Max Value :" + max)
+  val min = books.minBy(book => book.pages)
+  println("Min Value" + min)
+}
