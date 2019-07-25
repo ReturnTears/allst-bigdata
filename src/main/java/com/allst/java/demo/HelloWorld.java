1de9cc0c934860ae7d7be06ebc0d43c0b3de858f
returntears <returntears@outlook.com>
2019-07-26 00:28:43 +0800

    Scala作业

---
+++ src/main/java/com/allst/java/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.allst.java.demo;
+
+/**
+ * HelloWorld
+ * @author JUNN
+ * @since 2019-07-26 上午 12:09
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+    }
+}
