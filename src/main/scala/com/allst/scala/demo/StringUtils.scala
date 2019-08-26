1fec69f34ff3fb303c4d1d792d1b15b2ecda1e7d
returntears <returntears@outlook.com>
2019-08-26 23:10:15 +0800

    字符串, 对象中的隐式转换类,返回其他类型

--- src/main/scala/com/allst/scala/demo/StringUtils.scala
+++ src/main/scala/com/allst/scala/demo/StringUtils.scala
@@ -6,7 +6,16 @@ package com.allst.scala.demo
   * @since 2019-08-26 下午 10:39
   */
 object StringUtils {
+
   implicit class StringImprovements(val s: String) {
     def increment = s.map(c => (c + 1).toChar)
+    def decrement = s.map(c => (c - 1).toChar)
+    // 返回到String类型是显式声明的
+    def hideAll: String = s.replaceAll(".", "*")
+    def plusOne = s.toInt + 1
+    def asBoolean = s match {
+      case "0" | "zero" | "" | " " => false
+      case _ => true
+    }
   }
 }
