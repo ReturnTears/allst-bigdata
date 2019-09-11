1bae7dab9d57028c210cf233a9b77339b08156b0
returntears <returntears@outlook.com>
2019-09-12 00:12:33 +0800

    控制语句

--- src/main/scala/com/allst/scala/demo/InputParam.scala
+++ src/main/scala/com/allst/scala/demo/InputParam.scala
@@ -10,11 +10,28 @@ import scala.io.StdIn
   */
 object InputParam {
   def main(args: Array[String]): Unit = {
-    println("请输入姓名:")
+    /*println("请输入姓名:")
     val name = StdIn.readLine()
-    println("请输入姓名:")
-    val age = StdIn.readLine()
-    println("请输入姓名:")
-    val sal = StdIn.readLine()
+    println("请输入年龄:")
+    val age = StdIn.readInt()
+    println("请输入薪水:")
+    val sal = StdIn.readDouble()
+    printf("用户的信息为 name=%s age=%d sal=%.2f", name, age, sal)
+*/
+    val a = 22
+    val res = if (a > 18) {
+      println("Hello World")
+      a + 100
+      "True"
+    } else {
+      a
+    }
+    println(res)
+
   }
+
+  /**
+    * Scala中任意表达式都有返回值的, 也就意味着if else表达式其实是由返回结果的, 具体返回结果的值取决于满足条件的代码体的最后一行内容
+    */
+
 }
