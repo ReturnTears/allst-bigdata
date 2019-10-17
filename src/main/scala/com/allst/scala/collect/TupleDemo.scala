8555613eca07c337b359ac87ea07ebb07008b1db
returntears <returntears@outlook.com>
2019-10-17 23:24:41 +0800

    元组入门

---
+++ src/main/scala/com/allst/scala/collect/TupleDemo.scala
@@ -0,0 +1,23 @@
+package com.allst.scala.collect
+
+/**
+  * 元组
+  * @author YiYa
+  * @since 2019-10-17 下午 10:13
+  */
+object TupleDemo {
+  def main(args: Array[String]): Unit = {
+    // 创建tuple, 类型是Tuple4
+    // 为了高效的操作元组, 编译器根据元素的个数不同对应不同的元组类型, 分别对应Tuple1~Tuple22
+    val tuplea = (1, 2, 3, "Hi", true)
+    println(tuplea)
+
+    println(tuplea._1)  // 访问第一个元素, 从1开始
+    println(tuplea.productElement(1)) // 访问第一个元素
+
+    // 遍历元组
+    for (item <- tuplea.productIterator) {
+      println("item = " + item)
+    }
+  }
+}
